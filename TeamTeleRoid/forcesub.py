9ab0c492d5e5ec8d37349809aacaa05fa81d990f
jagan1857 <107793234+jagan1857@users.noreply.github.com>
2022-10-09 07:23:54 +0530

    Update forcesub.py

--- TeamTeleRoid/forcesub.py
+++ TeamTeleRoid/forcesub.py
@@ -13,7 +13,7 @@ async def ForceSub(bot: Client, cmd: Message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="You Are Ban To Use Me. Contact My [Owner](https://t.me/RoyalKrrishna).",
+                text="You Are Ban To Use Me. Contact My [Support](https://t.me/+cTsBLwDFiE44NzY1).",
                 parse_mode="markdown",
                 disable_web_page_preview=True
             )
@@ -47,7 +47,7 @@ async def ForceSub(bot: Client, cmd: Message):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Somthing Went Wrong. Contact My [Owner](https://t.me/RoyalKrrishna).",
+            text="Somthing Went Wrong. Contact My [Support](https://t.me/+cTsBLwDFiE44NzY1).",
             parse_mode="markdown",
             disable_web_page_preview=True
         )
